8523b71e283433046f7aa422f798b464ad59b484
Trent McNabb <trent@Trents-MacBook-Pro.local>
2021-05-23 21:23:54 +0000

    completed PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,15 +7,30 @@ Last modified May 23 2021
 import os
 import csv
 
+candidates = {}
+totalVotes = 0
+maxVotes = 0
 csvFile = os.path.join(".", "Resources", "election_data.csv" )
 with open(csvFile, 'r') as file:
     csvReader = csv.reader(file,delimiter=',')
     csvHeader = next(csvReader)
-    print(csvHeader)
+    for row in csvReader:
+        totalVotes = totalVotes+1
+        #if the candidates name is a key in the dictionary, increment the value by one. If not, insert the key with the value of 1
+        if row[2] in candidates:
+            candidates[row[2]]+=1
+        else:
+            candidates[row[2]] = 1
 
 
-output = "test"
-
+output = "Election Results\n------------------------\nTotal Votes: {:d}\n------------------------\n".format(totalVotes)
+for candidate in candidates:
+    votes = candidates[candidate]
+    output += "{:s}: {:.3%} ({:d})\n".format(candidate, votes/totalVotes, totalVotes)
+    if votes > maxVotes:
+        maxVotes = votes
+        winner = candidate
+output += "------------------------\nWinner: {:s}\n------------------------\n".format(winner)
 print(output)
 
 #output the string to a file
