3dacabde68f42891e70a7841d85f235a4ac63216
Trent McNabb <trent@Trents-MacBook-Pro.local>
2021-05-23 18:46:30 +0000

    outputs to file

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,8 +7,24 @@ Last modified May 23 2021
 import os
 import csv
 
-csv_file = os.path.join(".", "Resources", "budget_data.csv" )
-with open(csv_file, 'r') as file:
-    csv_reader = csv.reader(file,delimiter=',')
-    csv_header = next(csv_reader)
-    print(csv_header)
+csvFile = os.path.join(".", "Resources", "budget_data.csv" )
+totalMonths = 1
+total = 0
+biggestGainMonth= 'test 123'
+biggestGain = 0
+biggestLossMonth = 'test 123'
+biggestLoss = 0
+
+with open(csvFile, 'r') as file:
+    csvReader = csv.reader(file,delimiter=',')
+    csvHeader = next(csvReader)
+
+#create output string
+output = "Total Months: {:d}\nTotal : ${:d}\nAverageChange: {:f}\nGreatest increase in profits: {:s} ({:d})\nGreatest decrease in profits: {:s} ({:d})".format(totalMonths,total,total/totalMonths,biggestGainMonth,biggestGain,biggestLossMonth,biggestLoss)
+
+print(output)
+
+#output the string to a file
+outputFile = open("output.txt","w+")
+outputFile.write(output)
+outputFile.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,8 +7,18 @@ Last modified May 23 2021
 import os
 import csv
 
-csv_file = os.path.join(".", "Resources", "election_data.csv" )
-with open(csv_file, 'r') as file:
-    csv_reader = csv.reader(file,delimiter=',')
-    csv_header = next(csv_reader)
-    print(csv_header)
+csvFile = os.path.join(".", "Resources", "election_data.csv" )
+with open(csvFile, 'r') as file:
+    csvReader = csv.reader(file,delimiter=',')
+    csvHeader = next(csvReader)
+    print(csvHeader)
+
+
+output = "test"
+
+print(output)
+
+#output the string to a file
+outputFile = open("output.txt","w+")
+outputFile.write(output)
+outputFile.close()
